ef0049240061df71c356a35cce8e440548b60ed8
AldarUlyumdzhiev <aldarulyumdzhiev@yandex.ru>
2025-03-12 22:02:43 +0300

    FilmStorage checkstyle fix

--- src/main/java/ru/yandex/practicum/filmorate/storage/film/FilmStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/film/FilmStorage.java
@@ -7,6 +7,8 @@ import java.util.Collection;
 public interface FilmStorage {
     public Collection<Film> getAll();
     public Film getById(Long id);
+
     public Film create(Film film);
+
     public Film update(Film newFilm);
 }
