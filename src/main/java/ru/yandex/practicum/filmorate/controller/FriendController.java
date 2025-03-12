ae1e671df0fe9ceeeb832dd620421f8b553a894d
ald228 <aldar2003@mail.ru>
2025-03-12 21:20:55 +0300

    add user friends and film likes

---
+++ src/main/java/ru/yandex/practicum/filmorate/controller/FriendController.java
@@ -0,0 +1,40 @@
+package ru.yandex.practicum.filmorate.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+import ru.yandex.practicum.filmorate.model.User;
+import ru.yandex.practicum.filmorate.service.FriendService;
+
+import java.util.Collection;
+
+@RestController
+@RequestMapping("/users")
+public class FriendController {
+    private final FriendService friendService;
+
+    @Autowired
+    public FriendController(FriendService friendService) {
+        this.friendService = friendService;
+    }
+
+    @GetMapping("/{id}/friends")
+    public Collection<User> getFriends(@PathVariable Long id) {
+        return friendService.getFriends(id);
+    }
+
+    @GetMapping("/{id}/friends/common/{otherId}")
+    public Collection<User> getCommonFriends(@PathVariable Long id, @PathVariable Long otherId) {
+        return friendService.getCommonFriends(id, otherId);
+    }
+
+    @PutMapping("/{id}/friends/{friendId}")
+    public void addFriend(@PathVariable Long id, @PathVariable Long friendId) {
+        friendService.addFriend(id, friendId);
+    }
+
+    @DeleteMapping("/{id}/friends/{friendId}")
+    public void deleteFriend(@PathVariable Long id, @PathVariable Long friendId) {
+        friendService.deleteFriend(id, friendId);
+    }
+}
